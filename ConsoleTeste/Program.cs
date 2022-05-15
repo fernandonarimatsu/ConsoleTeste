028e8f78e59b01652a594d45d241d4b24dd3d151
Fernando Narimatsu <Fernando Narimatsu@DESKTOP-C8IP0J1>
2022-05-15 15:10:26 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleTeste/Program.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleTeste.SistemaBanco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTeste {
+    public class Program {
+        public static void Main(string[] args) {
+            var conta = new Conta();
+            conta.NomeConta = "Conta Teste";
+            conta.NumeroConta = 970069;
+            conta.Agencia = 4355;
+            conta.Saldo = 1000;
+
+            conta.Titular = new Titular() {
+                Cpf = "03904529986",
+                Rg = "49805594",
+                Endereco = "Rua Patagonia",
+                Nome = "Fernando"
+
+            };
+
+            Console.WriteLine("Olá " + conta.Titular.Nome);
+            Console.WriteLine();
+
+            Console.WriteLine("Seu saldo atual é R$: " + conta.VerSaldo());
+            Console.WriteLine("-----------------------------");
+
+            string sair;
+
+            do {
+
+                Console.WriteLine("Digite 1 para SAQUE \tDigite 2 para DEPÓSITO:");
+                int opcao = Convert.ToInt16(Console.ReadLine());
+                if (opcao == 1) {
+                    Console.WriteLine("Qual o valor do saque? ");
+                    conta.Sacar(Convert.ToDouble(Console.ReadLine()));
+
+                }
+                else {
+                    Console.WriteLine("Qual o valor do depósito? ");
+                    conta.Depositar(Convert.ToDouble(Console.ReadLine()));
+                    Console.WriteLine("-----------------------------");
+                }
+                Console.WriteLine("Seu saldo após o depósito é R$: " + conta.VerSaldo());
+                Console.WriteLine("-----------------------------");
+
+                Console.WriteLine("Deseja Sair? SIM - NAO");
+                sair = Console.ReadLine().ToUpper();
+
+            } while (sair != "SIM");
+            Console.Clear();
+            Console.WriteLine("Fim da execução.....");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleTeste/SistemaBanco/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTeste.SistemaBanco
+{
+    public class Conta
+    {
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public string NomeConta { get; set; }
+        public Titular Titular { get; set; }
+        public Double Saldo { get; set; }
+
+        public Double VerSaldo()
+        {
+            return Saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente.");
+            }
+        }
+    }
+}
